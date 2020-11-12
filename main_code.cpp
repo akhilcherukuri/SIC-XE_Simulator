4c0fda9663eceee6f20a21fa7d2689d17e24b6cd
jdeharo27 <35617050+jdeharo27@users.noreply.github.com>
2020-11-11 22:53:42 -0800

    Add files via upload
    Added validate_arguments() and populated temp_line_s

--- main_code.cpp
+++ main_code.cpp
@@ -7,6 +7,10 @@
 
 using namespace std;
 
+// Error Codes
+int ERR_INVALID_ARGS = -1;
+int SUCCESS = 0;
+
 void cli__init();
 // Intialization
 
@@ -22,14 +26,20 @@ void parse_sample_program_into_data_structure();
 void pass_1_assembly();
 
 bool check_validity();
-bool validate_three_arguments();
-bool validate_opcode();
+int validate_arguments(char *);
+int validate_opcode(char *);
 
 // Pass 2 Assembly Function Declarations
 
 void pass_2_assembly();
 
 // Data Structures
+struct temp_line_s
+{
+    string label;
+    string opcode;
+    string operand;
+};
 
 struct instruction_data_s
 {
@@ -47,6 +57,8 @@ struct instruction_data_s
         uint8_t n : 1;
     };
 };
+
+temp_line_s temp_line;
 
 vector<instruction_data_s> inst_v;
 
@@ -116,44 +128,119 @@ void fill_regtab()
     fclose(fp);
 }
 
-// // Pass 1 Assembly
+// Pass 1 Assembly
 
-// // Pass 1 Assembly Function Definitions
-// bool check_validity()
-// {
-//     if (validate_three_arguments())
-//     {
-//         if (validate_opcode())
-//         {
-//         }
-//     }
-// }
+// Pass 1 Assembly Function Definitions
+int validate_arguments(char * opcode_line)
+{
+    char label[20], opcode[20], operand[20];
 
-// void parse_sample_program_into_data_structure() // Include in pass 1
-// {
-//     FILE *fp = fopen("input_assembly_file.txt", "r");
-//     char opcode_line[100];
-//     while (fgets(opcode_line, sizeof(opcode_line), fp))
-//     {
-//         if (check_validity())
-//         {
-//             assign_address();
-//         }
-//     }
-// }
+    string strFromChar;
+    strFromChar.append(opcode_line);
+    istringstream ss(strFromChar);
 
-// void pass_1_assembly()
-// {
-//     parse_sample_program_into_data_structure();
-// }
+	string word;
+
+	int number_arg = 0;
+    bool no_label = false;
+    if(opcode_line[0] == ' ')
+    {
+        no_label = true;
+        number_arg++;
+    }
+    
+    int i = 0;
+    string arr_instruction[3];
+    // TODO: Handle case for assembler directives. 
+    while (ss >> word) 
+	{
+        if(no_label && i == 0)
+        {
+            arr_instruction[0] = "NA"; // Label
+            arr_instruction[1] = word; // Opcode
+            i += 2;
+            continue;
+        }
+        arr_instruction[i] = word;
+        i++;
+        number_arg++;
+	}
+    // TODO: Add debug macros
+    // cout << "Number of ARGS: " << number_arg << endl;
+
+    if (number_arg >= 4){
+        return ERR_INVALID_ARGS;
+    }
+
+    temp_line.label = arr_instruction[0];
+    temp_line.opcode = arr_instruction[1];
+    temp_line.operand = arr_instruction[2];
+
+    // cout << "Label " << temp_line.label << endl;
+    // cout << "Opcode " << temp_line.opcode << endl;
+    // cout << "Operand " << temp_line.operand << endl;
+
+    return SUCCESS;
+}
+
+bool check_validity(char *opcode_line)
+{
+    int err_code = validate_arguments(opcode_line);
+    if (err_code == 0)
+    {
+        // err_code = validate_opcode(opcode_line);  // TODO: Implement validate_opcode()
+        if (err_code == 0)
+        {
+            // valid instruction line
+        }
+        else
+        {
+            cout << "Invalid instruction ocurred. Code: " << err_code << endl;
+        }
+    }
+    else
+    {
+        // TODO: Error code mapping and error log file.
+        cout << "Error ocurred. Code: " << err_code << endl;
+    }
+}
+
+void parse_sample_program_into_data_structure() // Include in pass 1
+{
+    FILE *fp = fopen("input_assembly_file.txt", "r");
+    char opcode_line[100];
+    if(fp != NULL)
+    {
+        while (fgets(opcode_line, sizeof(opcode_line), fp))
+        {
+            if (check_validity(opcode_line))
+            {
+                //assign_address();
+            } // TODO: Handle error validity case
+        }
+        fclose(fp);
+    }
+    else
+    {
+        cout << "Error opening the file." << endl;
+    }
+}
+
+void pass_1_assembly()
+{
+    parse_sample_program_into_data_structure();
+}
 
 int main()
 {
+    // TODO: Check if assembler program starts with START and ends with END
     fill_optab();
     fill_regtab();
     cout << REGTAB["X"] << endl;
     cout << REGTAB["A"] << endl;
     cout << REGTAB["F"] << endl;
 
-    // parse_sample_program_into_data_structure();
+    pass_1_assembly();
+
+    return 0;
 }
